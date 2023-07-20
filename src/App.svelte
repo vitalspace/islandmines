ef3d2aa1c4b82bae4ee747b52574d4f33855ccec
vitalspace <myglobalsounds@gmail.com>
2023-07-20 00:04:51 -0600

    Hero Idea

--- src/App.svelte
+++ src/App.svelte
@@ -2,4 +2,18 @@
 
 </script>
 
-<h1>hello world</h1>
+<div class="absolute inset-0 h-screen bg-gradient-to-br from-[#e7b6cf] via-red-200 to-[#2cc5bd] text-white">
+    <header></header>
+    <section class="fixed inset-0 mx-auto flex items-center justify-around border-2 border-red-300 text-center">
+      <div class="flex w-96 flex-col space-y-4 text-left">
+        <p class="text-pink-500 font-bold">Launching Soon</p>
+        <h1 class="text-9xl">Island Mines</h1>
+        <p>Don’t miss out on the release of our new NFT. Sign up below to receive updates when we go live.</p>
+        <button class="rounded-full border-2 px-4 py-2 text-xl">Sing up</button>
+      </div>
+      <div>
+        <h1>ajdljsak</h1>
+      </div>
+    </section>
+  </div>
+  
