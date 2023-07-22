d488e966ad2d7906305a6feccf6b9481710502e4
vitalspace <myglobalsounds@gmail.com>
2023-07-21 19:17:00 -0600

    Added header

--- src/App.svelte
+++ src/App.svelte
@@ -5,12 +5,22 @@
 <div
   class="absolute inset-0 h-screen bg-gradient-to-br from-[#e7b6cf] via-red-200 to-[#2cc5bd] text-white"
 >
-  <header />
+  <header class="relative z-10">
+    <nav class="container mx-auto flex justify-end py-10">
+      <ul class="flex space-x-10 pr-32">
+        <li><a class="cursor-pointer" href="/">Home</a></li>
+        <li><a class="cursor-pointer" href="/">Play</a></li>
+        <li><a class="cursor-pointer" href="/">Buy $CIM</a></li>
+        <li><a class="cursor-pointer" href="/">Community</a></li>
+        <li><a class="cursor-pointer" href="/">Whitepaper</a></li>
+      </ul>
+    </nav>
+  </header>
   <section
     class="fixed inset-0 mx-auto flex items-center justify-around border-2 border-red-300 text-center"
   >
     <div class="flex w-96 flex-col space-y-4 text-left">
-      <p class="text-pink-500 font-bold">Launching Soon</p>
+      <p class="text-pink-100 font-bold">Launching Soon...</p>
       <h1 class="text-9xl">Island Mines</h1>
       <p>
         Don’t miss out on the release of our new NFT. Sign up below to receive
