e68c30fa4f51b77b60f50544750eb33e394d9c74
vitalspace <myglobalsounds@gmail.com>
2023-07-21 19:01:44 -0600

    added canvas island

--- src/App.svelte
+++ src/App.svelte
@@ -1,19 +1,25 @@
 <script lang="ts">
-
+  import Scene from "./componets/scene.svelte";
 </script>
 
-<div class="absolute inset-0 h-screen bg-gradient-to-br from-[#e7b6cf] via-red-200 to-[#2cc5bd] text-white">
-    <header></header>
-    <section class="fixed inset-0 mx-auto flex items-center justify-around border-2 border-red-300 text-center">
-      <div class="flex w-96 flex-col space-y-4 text-left">
-        <p class="text-pink-500 font-bold">Launching Soon</p>
-        <h1 class="text-9xl">Island Mines</h1>
-        <p>Don’t miss out on the release of our new NFT. Sign up below to receive updates when we go live.</p>
-        <button class="rounded-full border-2 px-4 py-2 text-xl">Sing up</button>
-      </div>
-      <div>
-        <h1>ajdljsak</h1>
-      </div>
-    </section>
-  </div>
-  
+<div
+  class="absolute inset-0 h-screen bg-gradient-to-br from-[#e7b6cf] via-red-200 to-[#2cc5bd] text-white"
+>
+  <header />
+  <section
+    class="fixed inset-0 mx-auto flex items-center justify-around border-2 border-red-300 text-center"
+  >
+    <div class="flex w-96 flex-col space-y-4 text-left">
+      <p class="text-pink-500 font-bold">Launching Soon</p>
+      <h1 class="text-9xl">Island Mines</h1>
+      <p>
+        Don’t miss out on the release of our new NFT. Sign up below to receive
+        updates when we go live.
+      </p>
+      <button class="rounded-full border-2 px-4 py-2 text-xl">Sing up</button>
+    </div>
+    <div id="test" class="cursor-pointer">
+      <Scene canvasWidth={800} canvasheith={500}/>
+    </div>
+  </section>
+</div>
